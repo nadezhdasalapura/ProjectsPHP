25c07e80af9342715e97bfae4798e0cebff28839
nadezhdasalapura <nadezhdasalapura@gmail.com>
2022-12-23 21:20:25 +0300

    homework3
    homework3

---
+++ homework--3 - копия.php
@@ -0,0 +1,78 @@
+<?php
+
+$a = 10;
+echo $a;
+
+?>
+
+<?php
+
+$a = 20;
+echo $a;
+
+?>
+
+<?php
+	$a = 1 + 2 + 3;
+	echo $a; 
+?>
+
+<?php
+	$a = 10;
+	$b = 5; 
+    $c = $a - $b;
+    $d = 7;
+    $result = $c + $d;
+    echo $result;
+?>
+
+<?php
+	$a = 1.5;
+	$b = 0.75; 
+    $result = $a + $b;
+    echo $result;
+?>
+
+<?php
+    $a = 5;
+	echo -$a; 
+
+?>
+
+<?php
+	$a = 10;
+	$b = 3;
+	echo $a % $b; 
+
+?>
+
+<?php
+	$str = "homework";
+	echo strlen($str);
+
+?>
+
+<?php
+	$isAdult = true;
+	var_dump($isAdult); 
+?>
+
+<?php
+	$isAdult = false;
+	var_dump($isAdult); // выведет true
+?>
+
+<?php
+	$test = null;
+	var_dump($test); 
+?>
+
+<?php
+ $n = 12345;
+ echo array_sum(str_split($n));
+?>
+
+<?php
+	echo 60 * 60 * 24 * 365; 
+?>
+
